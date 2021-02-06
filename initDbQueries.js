d4ccbff292c810a82055f98703daca1c1c098dc8
manisha1895 <manisha1895@gmail.com>
2021-02-06 15:54:07 +0530

    query update to avoid redundant inserts

--- initDbQueries.js
+++ initDbQueries.js
@@ -8,7 +8,13 @@ export const initDbQueries = {
     showTables: "SHOW TABLES;",
     setContext: "USE mysql;",
     createTable: "CREATE TABLE IF NOT EXISTS FRUITS (name VARCHAR(25));",
-    insertFruits: "INSERT INTO FRUITS (name) VALUES ('Mango'),('Orange'),('Grape'),('Pomegranate') WHERE NOT EXISTS (SELECT * FROM FRUITS);",
+    insertFruits: "INSERT INTO FRUITS (name) \n" +
+        "SELECT * FROM ( \n" +
+        "SELECT 'Mango' UNION ALL \n" +
+        "SELECT 'Orange' UNION ALL \n" +
+        "SELECT 'Grape' UNION ALL\n" +
+        "SELECT 'Pomegranate') AS DATA \n" +
+        "WHERE NOT EXISTS (SELECT * FROM FRUITS);",
     selectFruits: "SELECT * from FRUITS;"
 
 }
