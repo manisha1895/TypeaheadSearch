983801c6ffd47de82f689401f62c563388c20356
manisha1895 <manisha1895@gmail.com>
2021-02-06 15:20:47 +0530

    proxy for frontend requests , sql set up with node and local testing

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 This is a simple typeahead search.
 
 STEPS to run in local :
-1. install sql driver : `npm install mysql`
-2. 
+1. install npm packages : `npm i`
+2. install mysql driver in local from https://dev.mysql.com/downloads/mysql/
+3. configure mysql server credentials for connection as :   
+   username: root
+   password: test1234
+   OR update your local credentials in initDbQueries.dbConfig
+3. build webpack : `npm run build`
+4. start the local server :  `npm run start`
+   This will ensure the necessary tables and values exist for search.
+5. open dist/index.html in a browser
+6. The search is for a simple entity - fruit and can be extended for bigger objects.
+
+The dropdown options change with respect to input text and once selected will appear as a chip beside.
+Selected entities even though displayed in the dropdown will not appear as chip if already selected once.
--- server.js
+++ server.js
@@ -1,66 +1,64 @@
 import express from 'express';
 import mysql from 'mysql';
 import cors from 'cors';
-import { createProxyMiddleware } from 'http-proxy-middleware';
 
-import { initDbQueries } from './initDbQueries';
+import { initDbQueries , dbConfig} from './initDbQueries';
 
 
 const app = express();
 const port = 8080;
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'test1234'
+    host: dbConfig.host,
+    user: dbConfig.user,
+    password: dbConfig.password,
+    database: dbConfig.database
 });
 
-app.use(cors());
-app.use('/api', createProxyMiddleware({ target: 'http://localhost:8080', changeOrigin: true }));
-
-// function insertRecords(connection){
-//     connection.query(initDbQueries.insertFruits, function (err, result) {
-//         if (err) throw err;
-//         console.log("Records inserted");
-//         connection.end(function (error){
-//             if(error) console.log('failed to disconnect from DB');
-//             else console.log('Disconnected from DB');
-//         });
-//     });
-// }
-// function createTable(connection){
-//     connection.query(initDbQueries.setContext,function (err,result){
-//         if(err) console.log('failed to set context',err);
-//         connection.query(initDbQueries.createTable, function (err, result) {
-//             if (err) throw err;
-//             insertRecords(connection);
-//         });
-//     });
-// }
-// function createDatabase (connection){
-//     connection.connect(function(err) {
-//         if (err) console.log('connection error',err);
-//         else console.log("Connected!");
-//         connection.query(initDbQueries.createDb, function (err, result) {
-//             if (err) console.log('query error',err);
-//             createTable(connection);
-//         });
-//     });
-// }
-//
-// createDatabase(connection);
-
-app.get('/api/fruits', (req, res) => {
-    connection.connect(function(err) {
+function insertRecords(connection){
+    connection.query(initDbQueries.insertFruits, function (err, result) {
         if (err) throw err;
-        connection.query(initDbQueries.selectFruits, function (err, result, fields) {
-            if (err) throw err;
-            console.log('got fruits',result);
-            res.send(result);
+        console.log("Records inserted",result);
+    });
+}
+function createTable(connection){
+    connection.query(initDbQueries.setContext,function (err,result){
+        if(err) console.log('failed to set context',err);
+        connection.query(initDbQueries.createTable, function (err, result) {
+            if (err) console.log('failed to create table',err);
+            insertRecords(connection);
         });
     });
+}
+function createDatabase (connection){
+        connection.query(initDbQueries.showTables, function (err, result) {
+            if (err) console.log('query error',err);
+            console.log(result);
+            createTable(connection);
+        });
+
+}
+
+createDatabase(connection);
+
+app.use(cors({
+    origin: 'localhost:63342',
+    credentials: false
+}));
+
+app.get('/*',function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
+app.get('/api/fruits', (req, res) => {
+        connection.query(initDbQueries.selectFruits, function (err, result, fields) {
+            if (err) console.log('api query error',err);
+            console.log('got fruits',result,fields);
+            res.json(result);
+        });
+});
 app.listen(port, () => {
     console.log(`App server now listening to port ${port}`);
 });
